fa892c64203bc20a72d26d6aa6b173ffaf68e0b8
Shazzad501 <assazzad501@gmail.com>
2025-01-31 00:17:39 +0600

    next js loading component setup

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingPage = () => {
+  return (
+    <div className='min-h-screen min-w-screen flex items-center justify-center'>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default LoadingPage;
