5cd654822c700ee66e8456820bd1485328abb402
Shazzad501 <assazzad501@gmail.com>
2025-01-30 23:57:23 +0600

    next js dynamic route slug implement

---
+++ src/app/about/[...slugs]/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutSlugPage = ({params}) => {
+  const p = params;
+  return (
+    <div>
+      <h2>About Slug page</h2>
+    </div>
+  );
+};
+
+export default AboutSlugPage;
