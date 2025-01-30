175058d1b15b57641a7d512c0ca879b7a607d4be
Shazzad501 <assazzad501@gmail.com>
2025-01-30 23:47:19 +0600

    conditional navbar create

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,22 +1,31 @@
+"use client"
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 const Navbar = () => {
-  return (
-    <nav>
-      <ul className="flex gap-5 justify-center items-center">
-        <Link href='/'>
-          <li>Home</li>
-        </Link>
-        <Link href={'/services'}>
-          <li>Service</li>
-        </Link>
-        <Link href={'/about'}>
-          <li>About</li>
-        </Link>
-      </ul>
-    </nav>
-  );
+  const pathname = usePathname();
+
+  if(!pathname.includes('dashboard')){
+    return (
+      <nav>
+        <ul className="flex gap-5 justify-center items-center">
+          <Link href='/'>
+            <li>Home</li>
+          </Link>
+          <Link href={'/services'}>
+            <li>Service</li>
+          </Link>
+          <Link href={'/about'}>
+            <li>About</li>
+          </Link>
+        </ul>
+      </nav>
+    );
+  }
+  else{
+    return(<></>);
+  }
 };
 
 export default Navbar;
